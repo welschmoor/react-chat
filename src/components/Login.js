4113ea908685cab1dd81ad4378cfdd3b0ed9541e
Johannes Welsch <welschmoor@gmail.com>
2021-10-21 00:04:31 +0200

    firebase auth added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,17 @@
 
 import React from "react"
+import { auth, provider } from "../firebase.js" // provider is what you're signing in with (google ie)
+
 
 const Login = () => {
+
+    const login = () => {
+        auth.signInWithRedirect(provider)
+    }
+
     return (
         <div>
-
+            <button onClick={login}>Google Sign In</button>
         </div>
     )
 }
